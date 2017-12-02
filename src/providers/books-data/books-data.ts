fefc5874d80a9a7c0fde834b63b49204de376397
Rafael <rafahidalgo.83@gmail.com>
2017-12-02 22:22:03 +0100

    Reparación de bug: ya se puede buscar poniendo sólo el título del libro o el autor

--- src/providers/books-data/books-data.ts
+++ src/providers/books-data/books-data.ts
@@ -8,8 +8,14 @@ export class BooksDataProvider {
     console.log('Hello BooksDataProvider Provider');
   }
 
-  getBooks(title?: String, author?: String) {
-    return this.http.get(`https://www.googleapis.com/books/v1/volumes?q=${title}+inauthor:${author}&key=AIzaSyAGLh_QvOsVLGyX337USOQLeWyEj9LJHdw`);
+  getBooks(title: String, author: String) {
+    if (title && author) {
+      return this.http.get(`https://www.googleapis.com/books/v1/volumes?q=${title}+inauthor:${author}&key=AIzaSyAGLh_QvOsVLGyX337USOQLeWyEj9LJHdw`);
+    } else if (title && !author) {
+      return this.http.get(`https://www.googleapis.com/books/v1/volumes?q=${title}&key=AIzaSyAGLh_QvOsVLGyX337USOQLeWyEj9LJHdw`);
+    } else if (!title && author) {
+      return this.http.get(`https://www.googleapis.com/books/v1/volumes?q=inauthor:${author}&key=AIzaSyAGLh_QvOsVLGyX337USOQLeWyEj9LJHdw`);
+    }
     //return this.http.get("../../assets/data/data.json");
   }
 
