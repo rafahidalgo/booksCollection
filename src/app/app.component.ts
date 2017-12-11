1caf00d9225ba1e42a1258bb0c5dc487365b5a87
Rafael <rafahidalgo.83@gmail.com>
2017-12-11 12:45:36 +0100

    Añadido menú lateral con acceso a Login y Colección, y cerrar menú

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,28 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, MenuController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { AngularFireAuth } from 'angularfire2/auth';
 import { StorageProvider } from '../providers/storage/storage';
 import { TabsPage } from '../pages/tabs/tabs';
+import {HomePage} from '../pages/home/home';
+
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+
   rootPage: any;
+  homePage = HomePage;
 
   constructor(platform: Platform,
               statusBar: StatusBar,
               splashScreen: SplashScreen,
               angularFireAuth: AngularFireAuth,
-              private storageProvider: StorageProvider) {
+              private storageProvider: StorageProvider,
+              private menuCtrl: MenuController) {
     platform.ready().then(() => {
 
 
@@ -37,5 +42,15 @@ export class MyApp {
     });
 
   }
+
+  openPage(page: any) {
+    this.rootPage = page;
+    this.menuCtrl.close();
+  }
+
+  closeMenu() {
+    this.menuCtrl.close();
+  }
+
 }
 
